007c74d3bfe05f8827161e084709d1d575b29b57
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-10-29 21:46:09 -0300

    Programa consultando dados de conta bancaria

---
+++ Exceptions1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Exceptions1.Entities;
+using System.Globalization;
+using Exceptions1.Entities.Exceptions;
+
+namespace Exceptions1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, balance, withDrawLimit);
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.WithDraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
